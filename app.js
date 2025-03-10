0ef33d47895f1d4d131c4c6d6c6a8d001f453544
samina-shameem <samina.shameem.clt@gmail.com>
2025-03-10 10:43:53 +0100

    variables-- declared variable.

---
+++ app.js
@@ -0,0 +1,36 @@
+//console.table({name: "samina", age:36});
+console.log( "samina");
+console.log( "samina");
+console.log( "samina");
+console.log( "samina");
+console.log( "samina");
+console.clear();
+
+console.log("samina");
+console.log("samina");
+
+//comments
+//console.log("age");
+
+//clg -->console.log
+//log -->console.log
+
+//variables
+//(reserved keyword)(variable name)(type of value)--format how write code
+//var,const , let--> key words
+
+/*declared variable*/
+//let name;
+//console.log(name);/*undefined bcs there is no value of variable  */
+
+let number=4;
+console.log(number);/*defined bcs variable has value */
+
+let name="samina salam";
+console.log(name);
+let age=36;
+console.log(age);
+
+//<--- assigning variable
+//let fruit;
+//fruit ="apple";
