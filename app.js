8013e23c36cd0b9c09566fbc7f353b30142ad62e
samina-shameem <samina.shameem.clt@gmail.com>
2025-03-11 11:09:48 +0100

    first challange of js course completed.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //console.table({name: "samina", age:36});
-console.log( "samina");
-console.log( "samina");
-console.log( "samina");
-console.log( "samina");
-console.log( "samina");
+console.log("samina");
+console.log("samina");
+console.log("samina");
+console.log("samina");
+console.log("samina");
 console.clear();
 
 console.log("samina");
@@ -23,15 +23,45 @@ console.log("samina");
 //let name;
 //console.log(name);/*undefined bcs there is no value of variable  */
 
-let number=4;
-console.log(number);/*defined bcs variable has value */
+let number = 4;
+console.log(number); /*defined bcs variable has value */
 
-let name="samina salam";
+let name = "samina salam";
 console.log(name);
-let age=36;
+let age = 36;
 console.log(age);
 
 //<--- assigning variable
 let fruit;
-fruit ="apple";
+fruit = "apple";
 console.log(fruit);
+
+/* conventtions of writing variable 
+every time creating variable not using reset keyword as the name
+to your variable bcs it give errors*/
+//using camel case format
+/*.you cannot write first letter as a number it causes to error
+  .don't use dash(-) between the names
+  .don't use dash(-)infront of the name its not valid.
+  .using underscore(_) its valid
+  .don't use _ infront of the name (may be work)
+  .first usin  $ sign is working
+  .
+*/
+
+let youtubChannel = "mrbeast";
+youtubChannel = "webdev";
+youtubChannel = "dev";
+console.log(youtubChannel);
+/*Use const when you don’t want a variable to change.
+  Use let for variables that may change. */
+
+  let name1="samina salam kalathil";
+  let whatDoYouWannaBecomeInYourLife="programmer";
+  let gender="female";
+  let twitterHandle="@samian.twitter";
+
+  console.log(name1);
+  console.log(whatDoYouWannaBecomeInYourLife);
+  console.log(gender);
+  console.log(twitterHandle);
